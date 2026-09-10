6783990e36aa3eccd1e69071dc3e23a9a7232459
MauroUCM <maumar03@ucm.es>
2026-09-10 15:58:02 +0200

    Agregar archivos de proyecto.

---
+++ EDA26-27/Ejercicios/Tema 0/Ej1/main001.cpp
@@ -0,0 +1,13 @@
+// Mauro Martinez Montes
+// EDA33
+// Coste: Constante O(1)
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello world!";
+
+	return 0;
+}
---
+++ EDA26-27/Ejercicios/Tema 0/Ej3/main003.cpp
@@ -0,0 +1,49 @@
+// Mauro Martinez Montes
+// EDA33
+// Coste: Lineal O(n)
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int dato;
+
+    cin >> dato;
+
+    if (dato == 0)
+        return false;
+
+    // escribir sol
+    cout << dato * 2 << "\n";
+
+    return true;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("Ejercicios/Tema 0/Ej3/1.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
